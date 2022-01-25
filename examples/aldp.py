e8f199048615f86f98d8a2e61f41ca8ef68d7da3
Vincent Stimper <vincent.stimper@gmail.com>
2022-01-25 21:14:39 +0100

    Correct sampling for evaluation

--- examples/aldp.py
+++ examples/aldp.py
@@ -212,17 +212,16 @@ for it in range(start_iter, max_iter):
         """
 
         # Draw samples
-        model.eval()
         z_samples = torch.zeros(0, 1)
         for i in range(eval_batches):
             if i == eval_batches - 1:
                 ns = ((eval_samples - 1) % batch_size) + 1
             else:
                 ns = batch_size
-            z_ = model.annealed_importance_sampler.sample_and_log_weights(ns,
-                                                                          logging=False)[0]
-            z_samples = torch.concat((z_samples, z_))
-        model.train()
+            with torch.no_grad():
+                z_ = model.annealed_importance_sampler.sample_and_log_weights(ns,
+                                                                              logging=False)[0]
+                z_samples = torch.concat((z_samples, z_.detach()))
 
         # Evaluate model and save plots
         evaluateAldp(z_samples, test_data, model.flow.log_prob,
