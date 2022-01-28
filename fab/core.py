c3c09808bcd92b2216c87d5e4812203fa5e5da86
lollcat <l.midgley@instadeep.com>
2022-01-28 09:19:32 +0000

    small changes [remove log expected prob loss, refactor target imports]

--- fab/core.py
+++ fab/core.py
@@ -46,8 +46,6 @@ class FABModel(Model):
             return self.fab_forward_kl(batch_size)
         elif self.loss_type == "sample_log_prob":
             return self.fab_sample_log_prob(batch_size)
-        elif self.loss_type == "log_expected_prob":
-            return self.fab_log_expected_prob(batch_size)
         else:
             raise NotImplementedError
 
