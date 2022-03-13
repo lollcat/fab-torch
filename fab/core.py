fix: stabilise forward kl loss

fab/core.py
@@ -74,8 +74,9 @@ class FABModel(Model):
         x_ais, log_w_ais = self.annealed_importance_sampler.sample_and_log_weights(batch_size)
         x_ais = x_ais.detach()
         log_w_ais = log_w_ais.detach()
+        w_ais = torch.softmax(log_w_ais, dim=0)
         log_q_x = self.flow.log_prob(x_ais)
-        return - torch.mean(torch.exp(log_w_ais) * log_q_x)
+        return - torch.mean(w_ais * log_q_x)
 
     def fab_sample_log_prob(self, batch_size: int, sample_frac: float = 1.0) -> torch.Tensor:
         """Compute FAB loss by maximising the log prob of ais samples under the flow."""
@@ -111,10 +112,11 @@ class FABModel(Model):
                    path)
 
     def load(self,
-             path: "str"
+             path: "str",
+             map_location: Optional[str] = None,
              ):
         """Load FAB model from file."""
-        checkpoint = torch.load(path)
+        checkpoint = torch.load(path, map_location=map_location)
         try:
             self.flow._nf_model.load_state_dict(checkpoint['flow'])
         except RuntimeError: