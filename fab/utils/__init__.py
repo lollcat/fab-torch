1be46824ce2edb4afb9afa7ba9cc4a2ed28289d9
lollcat <l.midgley@instadeep.com>
2022-03-15 12:47:37 +0000

    fix: util init

---
+++ fab/utils/__init__.py
@@ -0,0 +1,4 @@
+from fab.utils.logging import WandbLogger, ListLogger
+from fab.utils.numerical import MC_estimate_true_expectation, effective_sample_size, \
+    importance_weighted_expectation
+from fab.utils.plotting import plot_history, plot_contours, plot_marginal_pair
