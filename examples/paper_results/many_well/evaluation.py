fd9e56c8622772da4a2f9f870927d1824b171739
laurence <l.midgley@instadeep.com>
2022-06-29 11:55:59 +0100

    fix: many well evaluation

--- examples/paper_results/many_well/evaluation.py
+++ examples/paper_results/many_well/evaluation.py
@@ -1,7 +1,7 @@
 import hydra
 from examples.setup_run_snf import make_normflow_snf_model, SNFModel
 
-from fab.target_distributions.gmm import GMM
+from fab.target_distributions.many_well import ManyWellEnergy
 import pandas as pd
 import os
 from omegaconf import DictConfig
@@ -37,12 +37,28 @@ def load_model(cfg: DictConfig, target, model_name: str):
         checkpoint = torch.load(path_to_model, map_location="cpu")
         flow._nf_model.load_state_dict(checkpoint['flow'])
 
-        transition_operator = Metropolis(n_transitions=cfg.fab.n_intermediate_distributions,
-                                         n_updates=cfg.fab.transition_operator.n_inner_steps,
-                                         adjust_step_size=True)
+        # transition_operator = HamiltonianMonteCarlo(
+        #     n_ais_intermediate_distributions=cfg.fab.n_intermediate_distributions,
+        #     n_outer=1,
+        #     epsilon=1.0,
+        #     L=cfg.fab.transition_operator.n_inner_steps,
+        #     dim=dim,
+        #     step_tuning_method="p_accept")
+
+        # model = FABModel(flow=flow,
+        #          target_distribution=target,
+        #          n_intermediate_distributions=cfg.fab.n_intermediate_distributions,
+        #          transition_operator=transition_operator,
+        #          loss_type=cfg.fab.loss_type)
+
+        # No need to do AIS, as we aren't interested in metrics after AIS for now.
+        transition_operator = Metropolis(n_transitions=1,
+                                         n_updates=1,
+                                         adjust_step_size=False)
+
         model = FABModel(flow=flow,
                  target_distribution=target,
-                 n_intermediate_distributions=cfg.fab.n_intermediate_distributions,
+                 n_intermediate_distributions=1,
                  transition_operator=transition_operator,
                  loss_type=cfg.fab.loss_type)
     return model
@@ -52,39 +68,40 @@ def load_model(cfg: DictConfig, target, model_name: str):
 def evaluate(cfg: DictConfig, model_name: str, num_samples=int(1e4)):
     torch.set_default_dtype(torch.float32)
     torch.manual_seed(cfg.training.seed)
-    target = GMM(dim=cfg.target.dim, n_mixes=cfg.target.n_mixes,
-                 loc_scaling=cfg.target.loc_scaling, log_var_scaling=cfg.target.log_var_scaling,
-                 use_gpu=False, n_test_set_samples=num_samples)
+    target = ManyWellEnergy(cfg.target.dim, a=-0.5, b=-6, use_gpu=False)
     if cfg.training.use_64_bit:
         torch.set_default_dtype(torch.float64)
         target = target.double()
     model = load_model(cfg, target, model_name)
-    eval = model.get_eval_info(num_samples, 500)
+    with torch.no_grad():
+        eval = model.get_eval_info(num_samples, 500)
     return eval
 
 
 @hydra.main(config_path="./", config_name="config.yaml")
 def main(cfg: DictConfig):
-    model_names = ["fab_buffer", "fab_no_buffer", "flow_kld", "flow_nis", "snf"]
+    model_names = ["fab_buffer", "fab_no_buffer", "snf"] # ["fab_buffer", "fab_no_buffer", "flow_kld", "flow_nis", "snf"]
     seeds = [1, 2, 3]
-    num_samples = int(500)
+    num_samples = int(5e4)
 
     results = pd.DataFrame()
     for model_name in model_names:
         for seed in seeds:
             name = model_name + f"_seed{seed}"
+            print(f"get results for {name}")
             eval_info = evaluate(cfg, name, num_samples)
             eval_info.update(seed=seed,
                              model_name=model_name)
             results = results.append(eval_info, ignore_index=True)
 
+    keys = ["eval_ess_flow", 'test_set_ais_mean_log_prob', 'test_set_modes_mean_log_prob']
     print("\n *******  mean  ********************** \n")
-    print(results.groupby("model_name").mean()[["eval_ess_flow", "eval_ess_ais", "test_set_mean_log_prob"]])
+    print(results.groupby("model_name").mean()[keys].to_latex())
     print("\n ******* std ********************** \n")
-    print(results.groupby("model_name").std()[["eval_ess_flow", "eval_ess_ais", "test_set_mean_log_prob"]]*1.96)
-    results.to_csv(open("/examples/paper_results/gmm/gmm_results.csv", "w"))
+    print((results.groupby("model_name").std()[keys]*1.96).to_latex())
+    results.to_csv(open("/home/laurence/work/code/FAB-TORCH/examples/paper_results/many_well/many_well_results.csv", "w"))
     print("overall results")
-    print(results[["model_name", "seed", "eval_ess_flow", "eval_ess_ais", "test_set_mean_log_prob"]])
+    print(results[["model_name", "seed"] + keys])
 
 
 if __name__ == '__main__':
