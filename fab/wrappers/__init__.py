01b0faf3e46a402668bb788e076b990cc0f4eeb4
laurence <l.midgley@instadeep.com>
2022-07-26 19:26:22 +0100

    feat: further changes after merge for normflow library update

--- fab/wrappers/__init__.py
+++ fab/wrappers/__init__.py
@@ -1,3 +1,3 @@
-from fab.wrappers.normflows import WrappedNormFlowModel
+from fab.wrappers.normflow import WrappedNormFlowModel
 from fab.wrappers.nflow import WrappedNFlowsModel
 from fab.wrappers.torch import WrappedTorchDist
