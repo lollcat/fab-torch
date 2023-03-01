3fc91bd1b6db8df4ba1313df951f8b1626929397
lollcat <laurencemidgley@gmail.com>
2023-03-01 14:52:30 +0000

    feat: playing with many well

--- experiments/many_well/ground_truth_higher_order_marginals.py
+++ experiments/many_well/ground_truth_higher_order_marginals.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import torch
 
 from fab.target_distributions.many_well import ManyWellEnergy
 from fab.utils.plotting import plot_marginal_pair
 
 
 if __name__ == '__main__':
+    torch.manual_seed(3)
     dist = ManyWellEnergy(dim=32)
-    samples = dist.sample((int(1e4),))
+    samples = dist.sample((int(2e4),))
     double_well_marginal = samples[:, np.arange(16)*2]
     plot_marginal_pair(double_well_marginal)
     plt.show()
@@ -18,13 +19,15 @@ if __name__ == '__main__':
     # Plot rate of sampling fainter modes.
     n_light_mode = np.sum(double_well_light_marginal_per_dim.numpy(), axis=-1)
     plt.hist(n_light_mode, density=True, log=True, bins=np.arange(16+1))
-    plt.xlabel("number of dim containing light mode")
+    plt.xlabel("number of dim containing light mode (out of the 16 bimodal dim)")
+    plt.ylabel("normalized frequency of samples")
+    plt.title("Exact samples using rejection sampling")
     plt.show()
 
 
     # Plot higher order marginal.
     # Get marginal for the first two being light.
-    marginal_condition = (double_well_light_marginal_per_dim[:, -1] == True) & (double_well_light_marginal_per_dim[:, -2] == True)
+    marginal_condition = (double_well_light_marginal_per_dim[:, 0] == True) & (double_well_light_marginal_per_dim[:, 1] == True)
     marginal_samples = double_well_marginal[marginal_condition]
     n_rows = 4
     fig, axs = plt.subplots(n_rows, n_rows, sharex=True, sharey=True, figsize=(n_rows * 3, n_rows * 3))
